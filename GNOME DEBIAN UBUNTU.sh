1e01ef0ac4d5a4acf219614d919fb868b11c4121
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2023-02-21 19:22:15 +0800

    .

--- GNOME DEBIAN UBUNTU.sh
+++ GNOME DEBIAN UBUNTU.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Install Flatpak and GNOME Software plugin for Flatpak
-sudo apt install xclip flatpak gnome-software-plugin-flatpak
+sudo apt install xclip flatpak gnome-software-plugin-flatpak -y
+sudo apt-get remove thunderbird -y
 
 # Add Flathub repository as a remote source
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
