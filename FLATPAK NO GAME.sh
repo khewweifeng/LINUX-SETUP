f66e0efe2e1a142f8054308ebe8295083fb38231
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2023-02-24 20:10:34 +0800

    org.raspberrypi.rpi-imager

--- FLATPAK NO GAME.sh
+++ FLATPAK NO GAME.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get remove thunderbird -y
 
-flatpak install com.discordapp.Discord org.videolan.VLC org.onlyoffice.desktopeditors com.obsproject.Studio com.github.hluk.copyq org.gnome.gitlab.YaLTeR.VideoTrimmer org.onionshare.OnionShare com.jgraph.drawio.desktop com.parsecgaming.parsec app.riftshare.RiftShare org.telegram.desktop flathub com.rtosta.zapzap com.github.unrud.VideoDownloader com.anydesk.Anydesk com.zettlr.Zettlr io.brackets.Brackets com.github.tenderowl.frog org.gnome.meld app.drey.Warp com.skype.Client com.microsoft.Teams com.getmailspring.Mailspring org.freedownloadmanager.Manager org.jdownloader.JDownloader -y
+flatpak install com.discordapp.Discord org.videolan.VLC org.onlyoffice.desktopeditors com.obsproject.Studio com.github.hluk.copyq org.gnome.gitlab.YaLTeR.VideoTrimmer org.onionshare.OnionShare com.jgraph.drawio.desktop com.parsecgaming.parsec app.riftshare.RiftShare org.telegram.desktop flathub com.rtosta.zapzap com.github.unrud.VideoDownloader com.anydesk.Anydesk com.zettlr.Zettlr io.brackets.Brackets com.github.tenderowl.frog org.gnome.meld app.drey.Warp com.skype.Client com.microsoft.Teams com.getmailspring.Mailspring org.freedownloadmanager.Manager org.jdownloader.JDownloader org.raspberrypi.rpi-imager -y
 
 sudo reboot
