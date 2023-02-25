f1c5814d373eb87020e8008d9e0165300071025c
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2023-02-25 17:23:25 +0800

    ENDLESS OS

---
+++ ENDLESS OS.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+flatpak install com.google.Chrome com.discordapp.Discord com.valvesoftware.Steam org.videolan.VLC net.lutris.Lutris com.heroicgameslauncher.hgl org.onlyoffice.desktopeditors com.obsproject.Studio com.github.hluk.copyq org.gnome.gitlab.YaLTeR.VideoTrimmer org.onionshare.OnionShare com.jgraph.drawio.desktop com.parsecgaming.parsec app.riftshare.RiftShare org.telegram.desktop flathub com.rtosta.zapzap com.github.unrud.VideoDownloader com.anydesk.Anydesk com.zettlr.Zettlr io.brackets.Brackets com.github.tenderowl.frog org.gnome.meld app.drey.Warp com.skype.Client com.microsoft.Teams com.getmailspring.Mailspring org.freedownloadmanager.Manager org.jdownloader.JDownloader org.raspberrypi.rpi-imager org.gnome.SimpleScan us.zoom.Zoom com.system76.Popsicle com.visualstudio.code org.gnome.World.PikaBackup com.belmoussaoui.Obfuscate org.kde.okular re.sonny.Junction org.kde.digikam com.github.gabutakut.gabutdm de.haeckerfelix.Shortwave org.shotcut.Shotcut fr.handbrake.ghb -y
+
+sudo reboot
