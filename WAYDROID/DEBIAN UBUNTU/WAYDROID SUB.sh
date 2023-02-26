952b50f0777001f1963925b6aebe9911c72986f3
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2023-02-26 20:46:47 +0800

    WAYDROID

---
+++ WAYDROID/DEBIAN UBUNTU/WAYDROID SUB.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+xdg-open https://github.com/casualsnek/waydroid_script
+
+sudo apt install lzip sqlite -y
+
+git clone https://github.com/casualsnek/waydroid_script
+cd waydroid_script
+sudo python3 -m pip install -r requirements.txt
+sudo python3 main.py [-i/-g/-n/-h/-w]
+
+sudo python3 main.py -g
+waydroid show-full-ui
--- WAYDROID/DEBIAN UBUNTU/WAYDROID.sh
+++ WAYDROID/DEBIAN UBUNTU/WAYDROID.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-xdg-open https://github.com/casualsnek/waydroid_script
+sudo apt install curl ca-certificates -y
+curl https://repo.waydro.id | sudo bash
+sudo apt install waydroid -y
 
-sudo apt install lzip sqlite -y
-
-git clone https://github.com/casualsnek/waydroid_script
-cd waydroid_script
-sudo python3 -m pip install -r requirements.txt
-sudo python3 main.py [-i/-g/-n/-h/-w]
-
-sudo python3 main.py -g
-waydroid show-full-ui
