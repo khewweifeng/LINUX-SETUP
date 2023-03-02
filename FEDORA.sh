02ad7324172498ae250f9a3f0baed32271eb3824
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2023-03-02 18:22:44 +0800

    Update FEDORA.sh

--- FEDORA.sh
+++ FEDORA.sh
@@ -3,6 +3,8 @@
 # Add Flathub repository as a remote source
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-sudo dnf install xclip neofetch gnome-tweaks -y
+sudo dnf install xclip neofetch -y
+
+flatpak install flathub org.gnome.Extensions -y
 
 sudo reboot
