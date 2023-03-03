22678a827e0bd0b52dae07a88a50be84d04354ce
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2023-03-03 22:41:58 +0800

    +
    io.github.hakandundar34coding.system-monitoring-center
    nload

--- FEDORA.sh
+++ FEDORA.sh
@@ -3,7 +3,7 @@
 # Add Flathub repository as a remote source
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-sudo dnf install xclip neofetch -y
+sudo dnf install xclip neofetch nload -y
 
 flatpak install flathub org.gnome.Extensions -y
 
--- GNOME DEBIAN UBUNTU.sh
+++ GNOME DEBIAN UBUNTU.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Flatpak and GNOME Software plugin for Flatpak
-sudo apt install neofetch xclip flatpak gnome-software-plugin-flatpak -y
+sudo apt install nload neofetch xclip flatpak gnome-software-plugin-flatpak -y
 sudo apt-get remove thunderbird -y
 
 # Add Flathub repository as a remote source
