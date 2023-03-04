9a138917abb5464462172a70fef4f618e5a95088
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2023-03-04 21:46:26 +0800

    Update FEDORA.sh

--- FEDORA.sh
+++ FEDORA.sh
@@ -3,7 +3,11 @@
 # Add Flathub repository as a remote source
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-sudo dnf install xclip neofetch nload -y
+sudo dnf install xclip neofetch nload chrome-remote-desktop fedora-workstation-repositories -y
+
+sudo dnf config-manager --set-enabled google-chrome
+
+sudo dnf install google-chrome-stable -y
 
 flatpak install flathub org.gnome.Extensions -y
 
