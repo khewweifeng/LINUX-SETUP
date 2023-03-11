008c4363672b3d18186498a033f6093bd72bbd09
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2023-03-11 23:54:39 +0800

    sudo apt remove thunderbird evolution -y

--- FEDORA.sh
+++ FEDORA.sh
@@ -3,7 +3,7 @@
 # Add Flathub repository as a remote source
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-sudo dnf remove thunderbird -y
+sudo dnf remove thunderbird evolution -y
 
 sudo dnf install xclip neofetch nload chrome-remote-desktop fedora-workstation-repositories chromium -y
 
--- FLATPAK.sh
+++ FLATPAK.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get remove thunderbird -y
+sudo apt remove thunderbird evolution -y
 
 flatpak install org.chromium.Chromium com.sindresorhus.Caprine com.discordapp.Discord com.valvesoftware.Steam org.videolan.VLC net.lutris.Lutris com.heroicgameslauncher.hgl org.onlyoffice.desktopeditors com.obsproject.Studio com.github.hluk.copyq org.gnome.gitlab.YaLTeR.VideoTrimmer org.onionshare.OnionShare com.jgraph.drawio.desktop com.parsecgaming.parsec app.riftshare.RiftShare org.telegram.desktop flathub com.rtosta.zapzap com.github.unrud.VideoDownloader com.anydesk.Anydesk com.zettlr.Zettlr io.brackets.Brackets com.github.tenderowl.frog org.gnome.meld app.drey.Warp com.skype.Client com.microsoft.Teams com.getmailspring.Mailspring org.freedownloadmanager.Manager org.jdownloader.JDownloader org.raspberrypi.rpi-imager com.visualstudio.code com.usebottles.bottles org.kde.filelight io.github.shiftey.Desktop io.github.jacalz.rymdport app.drey.Warp io.github.hakandundar34coding.system-monitoring-center -y
 
