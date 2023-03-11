58f2f0d3f63b4bb5a3b74853b522eaab04430c04
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2023-03-11 21:19:56 +0800

    Update GNOME DEBIAN UBUNTU.sh

--- GNOME DEBIAN UBUNTU.sh
+++ GNOME DEBIAN UBUNTU.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Install Flatpak and GNOME Software plugin for Flatpak
+sudo apt update
 sudo apt install nload neofetch xclip flatpak gnome-software-plugin-flatpak -y
 sudo apt-get remove thunderbird -y
 
 # Add Flathub repository as a remote source
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+sudo apt update
+sudo apt upgrade -y
 
 sudo reboot
