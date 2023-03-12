27fa441aee67cb3b37dfc40994d7ec2bb0121704
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2023-03-12 13:08:04 +0800

    UBUNTU 22.04

--- GNOME UBUNTU 22.04.sh
+++ GNOME UBUNTU 22.04.sh
@@ -4,8 +4,9 @@
 sudo apt install nload wget neofetch xclip flatpak gnome-software-plugin-flatpak -y
 sudo apt remove thunderbird evolution aisleriot five-or-more hitori gnome-sudoku gnome-tetravex -y
 sudo dpkg --add-architecture i386 && sudo apt update && sudo apt install -y libasound2-plugins:i386 libsdl2-2.0-0:i386 libdbus-1-3:i386 libsqlite3-0:i386
+sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bullseye/winehq-bullseye.sources
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
 sudo apt update
 sudo apt install --install-recommends winehq-staging -y
 
