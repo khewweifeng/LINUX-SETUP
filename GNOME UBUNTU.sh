30fdb71ab24a09afde1b13935278d29d378654a7
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2023-03-12 13:04:48 +0800

    +

---
+++ GNOME UBUNTU.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install Flatpak and GNOME Software plugin for Flatpak
+sudo apt install nload wget neofetch xclip flatpak gnome-software-plugin-flatpak -y
+sudo apt remove thunderbird evolution aisleriot five-or-more hitori gnome-sudoku gnome-tetravex -y
+sudo dpkg --add-architecture i386 && sudo apt update && sudo apt install -y libasound2-plugins:i386 libsdl2-2.0-0:i386 libdbus-1-3:i386 libsqlite3-0:i386
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bullseye/winehq-bullseye.sources
+sudo apt update
+sudo apt install --install-recommends winehq-staging -y
+
+
+# Add Flathub repository as a remote source
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+sudo apt update
+sudo apt upgrade -y
+
+sudo reboot
