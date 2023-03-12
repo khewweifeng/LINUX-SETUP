6653e924ed457776651cd0548b4e1c55424490dc
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2023-03-12 11:33:15 +0800

    Update GNOME DEBIAN UBUNTU.sh
    sudo dpkg --add-architecture i386 && sudo apt update && sudo apt install -y wine64 wine32 libasound2-plugins:i386 libsdl2-2.0-0:i386 libdbus-1-3:i386 libsqlite3-0:i386

--- GNOME DEBIAN UBUNTU.sh
+++ GNOME DEBIAN UBUNTU.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Flatpak and GNOME Software plugin for Flatpak
-sudo apt update
+sudo dpkg --add-architecture i386 && sudo apt update && sudo apt install -y wine64 wine32 libasound2-plugins:i386 libsdl2-2.0-0:i386 libdbus-1-3:i386 libsqlite3-0:i386
 sudo apt install nload ibus-pinyin neofetch xclip flatpak gnome-software-plugin-flatpak -y
 sudo apt remove thunderbird evolution -y
 
