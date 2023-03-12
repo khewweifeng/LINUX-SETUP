5fb0c2ee418b2fd769d127934daa52dca27a85a6
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2023-03-12 23:09:13 +0800

    remove thunderbird evolution aisleriot five-or-more hitori gnome-sudoku gnome-tetravex -y

--- FLATPAK NG.sh
+++ FLATPAK NG.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt remove thunderbird evolution -y
+sudo apt remove thunderbird evolution aisleriot five-or-more hitori gnome-sudoku gnome-tetravex -y
 
 flatpak install org.chromium.Chromium com.sindresorhus.Caprine com.discordapp.Discord org.videolan.VLC org.onlyoffice.desktopeditors com.obsproject.Studio com.github.hluk.copyq org.gnome.gitlab.YaLTeR.VideoTrimmer org.onionshare.OnionShare com.jgraph.drawio.desktop com.parsecgaming.parsec app.riftshare.RiftShare org.telegram.desktop flathub com.rtosta.zapzap com.github.unrud.VideoDownloader com.anydesk.Anydesk com.zettlr.Zettlr io.brackets.Brackets com.github.tenderowl.frog org.gnome.meld app.drey.Warp com.skype.Client com.microsoft.Teams com.getmailspring.Mailspring org.freedownloadmanager.Manager org.jdownloader.JDownloader org.raspberrypi.rpi-imager us.zoom.Zoom com.visualstudio.code com.usebottles.bottles io.github.jacalz.rymdport app.drey.Warp io.github.hakandundar34coding.system-monitoring-center org.gnome.Boxes -y
 
